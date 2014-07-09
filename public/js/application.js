0808b86fc2de2e24842c74690ae7a10838826a14
Danny Glover <djglover00@gmail.com>
2014-07-08 20:34:20 -0700

    map on page, shadow and background styled

--- public/js/application.js
+++ public/js/application.js
@@ -4,4 +4,25 @@ $(document).ready(function() {
   // when we try to bind to them
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+
+
+  function initialize() {
+          var myLatlng = new google.maps.LatLng(37.758297, -122.425701);
+          var mapOptions = {
+            zoom: 12,
+            center: myLatlng,
+            minZoom:12
+          }
+          var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+          var marker = new google.maps.Marker({
+              position: myLatlng,
+              map: map,
+              animation: google.maps.Animation.DROP,
+              title: 'Hello World!'
+          });
+
+        }
+        google.maps.event.addDomListener(window, 'load', initialize);
 });
+
+
